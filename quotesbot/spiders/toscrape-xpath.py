bf8c27a037eb12c73df46a7105a88b67e690cfd1
AFM Sayem <afm.sayem@gmail.com>
2017-12-09 13:40:40 +0800

    crawler(deviantart): create deviant tag spider

--- quotesbot/spiders/toscrape-xpath.py
+++ quotesbot/spiders/toscrape-xpath.py
@@ -3,17 +3,20 @@ import scrapy
 
 
 class ToScrapeSpiderXPath(scrapy.Spider):
-    name = 'toscrape-xpath'
-    start_urls = [
-        'http://quotes.toscrape.com/',
-    ]
+    name = 'deviant'
+
+    def __init__(self, tag, *args, **kwargs):
+        super(ToScrapeSpiderXPath, self).__init__(*args, **kwargs)
+        self.tag = tag
+        self.start_urls = ['https://www.deviantart.com/tag/%s' % tag]
+
+    # title, artist, created_at, tag, url, image
 
     def parse(self, response):
-        for quote in response.xpath('//div[@class="quote"]'):
+        for item in response.xpath('//span[@class="thumb"] | //span[@class="thumb wide"] | //span[@class="thumb narrow-thumb"]'):
             yield {
-                'text': quote.xpath('./span[@class="text"]/text()').extract_first(),
-                'author': quote.xpath('.//small[@class="author"]/text()').extract_first(),
-                'tags': quote.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').extract()
+                'url': item.xpath('./a[@class="torpedo-thumb-link"]/@href').extract_first(),
+                'image': item.xpath('./a[@class="torpedo-thumb-link"]/img/@src').extract_first(),
             }
 
         next_page_url = response.xpath('//li[@class="next"]/a/@href').extract_first()
